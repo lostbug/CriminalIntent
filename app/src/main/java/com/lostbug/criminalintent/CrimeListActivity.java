95fb120749ea7022002605db50b5c1b15ace6597
rockan007 <rockan007@gmail.com>
2017-09-25 22:34:38 +0800

    crime list

---
+++ app/src/main/java/com/lostbug/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.lostbug.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
